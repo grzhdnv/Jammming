ff2e52813f58dc5cac3666b8ae3c01378c6b39aa
mgrzhdnv <mgrazhdanov@gmail.com>
2022-03-03 15:43:25 +0300

    PlayList name change and Save button

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,8 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -37,6 +39,14 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
     <div>
@@ -47,7 +57,9 @@ class App extends React.Component {
           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
           <Playlist playlistName={this.state.playlistName} 
             playlistTracks={this.state.playlistTracks}
-            onRemove={this.removeTrack} />
+            onRemove={this.removeTrack}
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist} />
         </div>
       </div>
     </div>
